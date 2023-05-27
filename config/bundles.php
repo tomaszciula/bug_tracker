692305c8d0fa6f926cb094bb5d15a8ef709115f6
Tomasz Ciuła <tomaszciula@macbook-air-tomasz.home>
2023-05-27 09:58:24 +0200

    auth

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
